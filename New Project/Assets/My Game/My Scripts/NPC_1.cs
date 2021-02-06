3c1c0098d01d37d023d2694e9472cb60a5571612
Nikitagontsrovtkvgee <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2021-02-07 00:58:44 +0200

    Мой YOuth Digital проект

---
+++ New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+
+		Speech.AddDialogue("0", "Welcome to our planet!", "1");
+
+
+
+		Speech.AddDialogue("1", "Yauuuu, fack you!", "2");
+		Speech.AddDialogue("2", "Ты не можешь перейти на 2 левел потому, что не собрал чертовы монеты))))Собери чертовы монеты а также способность и сможешь перейти на лвл 2");
+
+	}
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other ){
+		Speech.SetDialogue("0");
+
+	}
+}
---
+++ New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public int coins;
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {  
+			
+			Destroy(collided.gameObject);
+			coins++;
+
+		}
+	}
+}
